1b7f84882a72997c4500b80697d4d2daf96ad8ad
Patrick Young <patrickjyoung97@gmail.com>
2018-05-08 18:21:01 +1000

    Php things
    added the initialise document for the databases and added an ignore for a test file that should never be used on the website

---
+++ init.php
@@ -0,0 +1,88 @@
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$dbname = "csit214";
+// Create database
+$sql = "CREATE DATABASE " . $dbname;
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+$conn->close();
+
+$conn = new mysqli($servername, $username, $password,$dbname);
+// Create tables
+$sql = "CREATE TABLE USERS (
+Uid INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+userName VARCHAR(30) NOT NULL,
+firstName VARCHAR(30) NOT NULL,
+lastName VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+regDate TIMESTAMP
+)";
+if($conn->query($sql) === TRUE){
+	echo "\n\r USER Table created";
+}
+else {
+	echo "Error creating database: " . $conn->error;
+}
+
+$sql = "CREATE TABLE EVENTS (
+Eid INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+description TEXT,
+edate DATETIME,
+location VARCHAR(200),
+coordinatorid INT(6) UNSIGNED,
+FOREIGN KEY (coordinatorid) REFERENCES USERS(Uid)
+)";
+if($conn->query($sql) === TRUE){
+	echo "\r\n EVENTS Table created ";
+}
+else {
+	echo "Error creating table: " . $conn->error;
+}
+
+$sql = "CREATE TABLE BOOKINGS (
+Bid INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Uid INT(6) UNSIGNED,
+Eid INT(6) UNSIGNED,
+bookingDate TIMESTAMP,
+additionalInfo TEXT,
+FOREIGN KEY (Eid) REFERENCES EVENTS(Eid),
+FOREIGN KEY (Uid) REFERENCES USERS(Uid)
+)";
+if($conn->query($sql) === TRUE){
+	echo "\r\n BOOKINGS Table created";
+}
+else {
+	echo "Error creating table: " . $conn->error;
+}
+
+$sql = "CREATE TABLE ADMIN (
+Uid INT(6) UNSIGNED PRIMARY KEY,
+adminLvl INT(2),
+role VARCHAR(30),
+FOREIGN KEY(Uid) REFERENCES USERS(Uid)
+)";
+if($conn->query($sql) === TRUE){
+	echo "\r\n ADMIN Table created";
+}
+else {
+	echo "Error creating table: " . $conn->error;
+}
+
+
+
+
+?> 
