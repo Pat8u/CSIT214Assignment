9fe1f02bcdb6e63b137b78eb1b6fa9207d23a800
Patrick Young <patrickjyoung97@gmail.com>
2018-05-09 08:47:21 +1000

    Updated getEvent and added an example of its usage
    The getevent now only echos the first row since the select statement will only return one row anyway. This makes it parsable by javascript as can be seen in the example of its usage

--- getEvent.php
+++ getEvent.php
@@ -12,12 +12,15 @@ $conn = new mysqli($servername, $username, $password,$dbname);
 $sql = mysqli_query($conn,"SELECT title,description,edate,creationedate,location FROM EVENTS WHERE Eid = $Eid");
 $rows = array();
 
-while($r = mysqli_fetch_assoc($sql)){
+//we don't need this as the Eid is a primary key therefore there will only ever be one row in the result
+/*while($r = mysqli_fetch_assoc($sql)){
 	$rows[] = $r;
-}
+}*/
 
-echo json_encode($rows)
 
+
+echo json_encode(mysqli_fetch_assoc($sql));
+$conn -> close();
 
 
 
