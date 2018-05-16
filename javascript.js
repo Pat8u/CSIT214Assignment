84ca639c9a4299a59032acceaa913ba03819772b
Patrick Young <patrickjyoung97@gmail.com>
2018-05-16 18:38:50 +1000

    Added some changes
    Added the changes that jack made to index and added the javascript that brendan did
    
    Co-Authored-By: Jack Bucinskas <jack.bucinskas@gmail.com>

---
+++ javascript.js
@@ -0,0 +1,70 @@
+function loadeventList() { 
+	var xhttp = new XMLHttpRequest();
+  		xhttp.onreadystatechange = function() {
+    	   if (this.readyState == 4 && this.status == 200) {
+    			var eventJSON = JSON.parse(this.responseText);
+          var eventTablediv = document.getElementById("eventTablediv");
+          var eventTable = document.createElement("TABLE");
+          eventTablediv.appendChild(eventTable);
+ 
+          for(i = 0; i < eventJSON.length; i++){
+            var tablerow = document.createElement("tr");
+            var tablehead = document.createElement("th");
+            eventTable.appendChild(tablerow);
+            tablerow.appendChild(tablehead);
+            var textnode = document.createTextNode(eventJSON[i].title);
+            tablehead.appendChild(textnode);
+            var tablerow = document.createElement("tr");
+            var tablehead = document.createElement("td");
+            eventTable.appendChild(tablerow);
+            tablerow.appendChild(tablehead);
+            var textnode = document.createTextNode("Event Info:");
+            tablehead.appendChild(textnode);
+			
+            var tablerow = document.createElement("tr");
+            var tablehead = document.createElement("td");
+            eventTable.appendChild(tablerow);
+            tablerow.appendChild(tablehead);
+            var textnode = document.createTextNode("Event Title: " + eventJSON[i].title);
+            tablehead.appendChild(textnode);
+            var tablehead = document.createElement("td");
+            tablerow.appendChild(tablehead);
+            var textnode = document.createTextNode("Description: " + eventJSON[i].description);
+            tablehead.appendChild(textnode);
+			
+            var tablerow = document.createElement("tr");
+            var tablehead = document.createElement("td");
+            eventTable.appendChild(tablerow);
+            tablerow.appendChild(tablehead);
+            var textnode = document.createTextNode("Event Location: " + eventJSON[i].location);
+            tablehead.appendChild(textnode);
+            var tablehead = document.createElement("td");
+            tablerow.appendChild(tablehead);
+            var textnode = document.createTextNode("Event Date: " + eventJSON[i].edate);
+            tablehead.appendChild(textnode);
+          }
+    		}
+  		}
+  		xhttp.open("POST", "getEventlist.php", true);
+  		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  		xhttp.send();
+}
+
+function loadbookingpage(n){
+      //need to create a hidden form in order to send the get request to the booking page and to redirect the user
+      var hidForm, eventId;
+      hidForm = document.createElement('form');
+      hidForm.action = 'Bookingpage.php';
+      hidForm.method = 'get';
+      eventId = document.createElement('input');
+      eventId.type = 'hidden';
+      eventId.name = 'Eid';
+      eventId.value = n;
+      hidForm.appendChild(eventId);
+      document.getElementById('hidden_form_container').appendChild(hidForm);
+      hidForm.submit();
+    }
+
+function loadbookinglist(n){
+  window.location.replace("Bookinglist.html");
+}
