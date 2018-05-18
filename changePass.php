99ca396ced0e92af8389f96d93cad1fb09f51d22
BraydonThomas <38969533+BraydonThomas@users.noreply.github.com>
2018-05-18 18:07:16 +1000

    Update changePass.php

--- changePass.php
+++ changePass.php
@@ -5,7 +5,7 @@ session_start();
 $Uid = $_SESSION["Uid"];
 
 if(isset($_POST['submit'])){
-$uPass = ($_POST["newPass"]);
+$uPass = md5($_POST["newPass"]);
 
 $conn = new mysqli($servername, $username, $password,$dbname);
 $sql = "UPDATE USERS SET passWord = '$uPass' WHERE Uid = '$Uid'";
